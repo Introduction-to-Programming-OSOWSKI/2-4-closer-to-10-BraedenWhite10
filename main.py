4b72c3cad689c595c4706edb7cf6444436f4ea55
BraedenWhite10 <johndo@mystma.org>
2022-09-21 09:20:48 -0500

    6

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 def close10(x,y):
     if abs(x-10) < abs (y-10):
         return x
-    elif abs(y-10) < abs(y-10):
+    elif abs(y-10) < abs(x-10):
         return y
     else: 
         return (0)
